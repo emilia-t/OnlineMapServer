33798c9a78216cf328d34b4d969d6150593b3919
emilia-t <sophietwilight@qq.com>
2023-02-26 14:38:23 +0800

    1

--- config/Mysql_OZ4pTiFHZf.php
+++ config/Mysql_OZ4pTiFHZf.php
@@ -2,5 +2,5 @@
 //公用账号
 $mysql_public_server_address="localhost";
 $mysql_public_user="map_edit";
-$mysql_public_password="";
+$mysql_public_password="15012266896wang";
 $mysql_public_db_name="map";
